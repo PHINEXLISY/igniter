ed9351e58a299363e18988b2ec41037a4236c9f1
Rintaro <tolabulu@163.com>
2020-05-21 18:14:55 +0800

    opt-translate into Chinese（代码注释翻译成中文）

--- app/src/main/java/io/github/trojan_gfw/igniter/initializer/MainInitializer.java
+++ app/src/main/java/io/github/trojan_gfw/igniter/initializer/MainInitializer.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import io.github.trojan_gfw.igniter.Globals;
 
 /**
+ * 在主进程（默认进程）运行的初始化器
  * Initializer that runs in Main Process (Default process).
  */
 public class MainInitializer extends Initializer {
